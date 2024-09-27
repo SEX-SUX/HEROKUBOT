be738ec1d6f75b95e0f323f07bc7a0517e2aaec2
SEX-SUX <sachinxpapa@gmail.com>
2024-09-27 21:41:09 +0530

    HEROKUHELPBOT

---
+++ config.py
@@ -0,0 +1,19 @@
+
+import re
+
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = getenv("API_ID", "")
+
+API_HASH = getenv("API_HASH")
+
+BOT_TOKEN = getenv("BOT_TOKEN")
+
+HEROKU_API_KEY = getenv("HEROKU_API_KEY", "")
+
+OWNER_ID = getenv("OWNER_ID", "1808943146")
+    
